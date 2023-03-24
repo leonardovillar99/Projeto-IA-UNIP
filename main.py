c33ad9bcad3b66cdfe204bf243b400fd9d0dd7b3
Leonardo <l.fvillar@bol.com.br>
2023-03-23 22:52:27 -0300

    002-Atualizando Interface

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ width = 800
 height = 800
 
 # Variáveis que controlam o movimento do personagem no mapa
-x = 0
+x = (width / 2) - (40 / 2)
 y = 0
 
 # Setando as variáveis de altura e largura no método display.set_mode
@@ -21,11 +21,14 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('IA - Dragon Ball Z')
 
 while True:
+    screen.fill((0,0,0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
-        #pygame.draw.rect(screen, (255, 255, 0), (0,400,100,400))
-        scale = pygame.transform.scale
-        image = scale(pygame.image.load("src/images/map.png"),(400,400))
+        pygame.draw.rect(screen, (255, 0, 0), (x,y,40,50))
+        if y >= height:
+            y = 0
+        y = y + 1
+
         pygame.display.update()
